9ff0697da68909de825901e140f318e20bc74e71
liuqinglin <liuqinglin@cd.tuan800.com>
2016-04-15 18:44:21 +0800

    realized namespace for controllers

--- config/boot.js
+++ config/boot.js
@@ -5,7 +5,7 @@ var views = require("co-views");
 app.root = path.join(__dirname, "..");
 
 app.render = views(path.join(app.root, 'app/views'), { ext: 'ejs'});
-app.controllers = require(path.join(app.root, "./config/initialize/controllers"));
+app.controllers = require(path.join(app.root, "./config/initializer/controllers"));
 app.controller = function (controllerName) {
   //if (controllerName.slice(-3) != '.js') {
   //  controllerName += ".js";
